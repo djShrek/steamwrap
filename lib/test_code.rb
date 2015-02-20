61068fe4fba98f3b5ea7a8a4163e1aba143cecc9
djShrek <boho527@gmail.com>
2015-02-19 18:26:30 -0800

    removed test_code

--- lib/test_code.rb
+++
@@ -1,7 +0,0 @@
-require 'steamwrap'
-
-argz = {steam_key: "55BA1C088556CDF59A3B43120193700F", player_id:  76561198033544098}
-
-player_info = Steamwrap::SteamUser.new(argz)
-
-puts player_info.get_player_bans
